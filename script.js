53362d3d2be849421b22b09d98e53beb0f2353b7
M10Wolverine <alanzhan@my.yorku.ca>
2021-05-13 10:58:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/*
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.message').appendChild();
+// Here is your reward
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+*/
+let secretNum = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+//document.querySelector(".number").textContent = secretNum;
+
+document.querySelector(".again").addEventListener("click", function () {
+  secretNum = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = 20;
+  document.querySelector(".number").textContent = "?"; //originally ?
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+  if (!guess || guess < 1 || guess > 20) {
+    //Invalid input
+    document.querySelector(".message").textContent = "No / Invalid Number!";
+  } else if (guess === secretNum) {
+    //Correct Guess
+    document.querySelector(".message").textContent =
+      "Correct Number! Congratulations!";
+    document.querySelector(".number").textContent = secretNum;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+  } else if (guess !== secretNum) {
+    document.querySelector(".message").textContent =
+      guess < secretNum ? "Too Low!" : "Too High!";
+    if (score > 1) {
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lose!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
